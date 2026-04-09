77f3c69d50c7399c8997501bfac8fa9f1a3fdbc8
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-09 19:22:22 +0530

    quotes pagenation

--- web_scraper/quotes_scraper.py
+++ web_scraper/quotes_scraper.py
@@ -5,30 +5,42 @@ import os
 
 BASE_URL = "https://quotes.toscrape.com"
 
-def scrape_quotes():
-    response = requests.get(BASE_URL)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    quotes = soup.find_all("span", class_="text")
-    authors = soup.find_all("small", class_="author")
-
-    data = []
-
-    for i in range(min(len(quotes),5)):
-        data.append({
-            "quote": quotes[i].text,
-            "author": authors[i].text,
-            "length": len(quotes[i].text)
-        })
-
-    return data
-
-
-import os
-import json
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, "data", "quotes.json")
+
+
+def scrape_quotes():
+    url = BASE_URL
+    all_data = []
+    count = 1
+    while url:
+        if count==4:
+            break
+        print(f"Scraping: {url}")
+        
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        quotes = soup.find_all("span", class_="text")
+        authors = soup.find_all("small", class_="author")
+
+        for i in range(len(quotes)):
+            all_data.append({
+                "quote": quotes[i].text,
+                "author": authors[i].text,
+                "length": len(quotes[i].text),
+                "page": count
+            })
+
+        next_btn = soup.find("li", class_="next")
+        if next_btn:
+            next_link = next_btn.find("a")["href"]
+            url = BASE_URL + next_link
+        else:
+            url = None
+        count+=1
+    return all_data
+
 
 def save_to_json(data):
     os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
@@ -38,8 +50,9 @@ def save_to_json(data):
 
     print("Saved at:", DATA_PATH)
 
+
 if __name__ == "__main__":
-    print("Scraping started...")
+    print("Pagination scraping started...")
     data = scrape_quotes()
     save_to_json(data)
-    print("Data saved successfully!")
+    print(f"Total quotes scraped: {len(data)}")
