817c25b4c83e369fe3f2d098b0070b261a1c9bd0
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-09 21:06:55 +0530

    Backend automation

--- web_scraper/backend/app.js
+++ web_scraper/backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Quote from "../backend/models/quotes.model.js";
 import cors from "cors";
+import { exec } from "child_process";
 
 const app = express();
 
@@ -15,6 +16,18 @@ app.get("/quotes", async (req, res) => {
     console.log(error);
     res.status(500).json({ message: "Server error" });
   }
+});
+
+app.post("/scrape", (req, res) => {
+  exec("python ../quotes_scraper.py", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return res.status(500).json({ message: "Scraping failed" });
+    }
+
+    console.log(stdout);
+    res.status(200).json({ message: "Scraping completed successfully" });
+  });
 });
 
 export default app;
